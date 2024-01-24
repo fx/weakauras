2a8ab06b51dd6598f9f089fe8e5099c96d9d8164
Marian Rudzynski <marian@rudzyn.ski>
2024-01-23 22:39:10 -0800

    compress source and persist in URI hash

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as pako from "pako";
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -9,8 +10,32 @@ import {
 } from "@/components/ui/navigation-menu";
 import { Share } from "lucide-react";
 import Link from "next/link";
+import { useContext, useEffect } from "react";
+import { GlobalContext } from "../app/layout";
 
 export function Header() {
+  const { source, setSource } = useContext(GlobalContext);
+
+  useEffect(() => {
+    const input = new TextEncoder().encode(source);
+    const output = pako.deflate(input);
+    const base64 = Buffer.from(output, "binary").toString("base64");
+    window.location.hash = base64;
+    history.pushState(null, "", `#${base64}`);
+  }, [source]);
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      const input = Buffer.from(window.location.hash, "base64");
+      const output = pako.inflate(input);
+      setSource(new TextDecoder().decode(output));
+    };
+    window.addEventListener("hashchange", handleHashChange);
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
   return (
     <header className="dark text-white sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 mb-4 p-2">
       <div className="container flex items-baseline">
