b6ac35f2565f807311e276f652e8c87f513e2e74
Marian Rudzynski <marian@rudzyn.ski>
2024-09-08 18:04:49 -0700

    use instance variables for options, duh

--- public/node_spec.rb
+++ public/node_spec.rb
@@ -5,13 +5,26 @@ require './spec/spec_helper'
 RSpec.describe Node do
   describe 'option' do
     it 'allows setting and modifying the default' do
-      node = Node.new
       Node.option :foo, default: 'bar'
+      node = Node.new
       expect(node.options).to eq(foo: 'bar')
       node.instance_eval do
         foo 'baz'
       end
       expect(node.options).to eq(foo: 'baz')
+    end
+
+    it 'should allow setting options on the instance' do
+      Node.option :foo, default: 'bar'
+      node_one = Node.new
+      node_two = Node.new
+      expect(node_one.options).to eq(foo: 'bar')
+      expect(node_two.options).to eq(foo: 'bar')
+      node_one.instance_eval do
+        foo 'baz'
+      end
+      expect(node_one.options).to eq(foo: 'baz')
+      expect(node_two.options).to eq(foo: 'bar')
     end
   end
 end
