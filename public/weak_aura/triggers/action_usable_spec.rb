d856d7b8c36505069a3fdf9292bf0c98db0a7c6a
mr <marian@rudzyn.ski>
2024-09-21 14:49:44 -0700

    make sure we can pass kwargs

--- public/weak_aura/triggers/action_usable_spec.rb
+++ public/weak_aura/triggers/action_usable_spec.rb
@@ -5,13 +5,13 @@ require './spec/spec_helper'
 RSpec.describe Trigger::ActionUsable do
   it 'should accept spell_count and default to the equality operator' do
     trigger = Trigger::ActionUsable.new(spell_count: 1).as_json[:trigger]
-    expect(trigger[:spellCount]).to eq(1)
+    expect(trigger[:spellCount]).to eq('1')
     expect(trigger[:spellCount_operator]).to eq('==')
   end
 
   it 'should accept spell_count w/ gte operator' do
     trigger = Trigger::ActionUsable.new(spell_count: '>= 1').as_json[:trigger]
-    expect(trigger[:spellCount]).to eq(1)
+    expect(trigger[:spellCount]).to eq('1')
     expect(trigger[:spellCount_operator]).to eq('>=')
   end
 end
