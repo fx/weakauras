d2091b335aeb32c5e50ee5eec53e790bf3da69ff
Marian Rudzynski <marian@rudzyn.ski>
2024-09-28 02:18:04 +0000

    add trigger shortcuts for charges, etc.

--- public/examples/mage/frost.rb
+++ public/examples/mage/frost.rb
@@ -9,6 +9,25 @@ load spec: :frost_mage
 hide_ooc!
 
 dynamic_group 'Frost Mage WhackAuras' do
+  icon 'Ray of Frost' do
+    action_usable! do
+      aura 'Cryopathy' do
+        stacks '>= 2' do
+          glow!
+        end
+      end
+      # charges '>= 2' do
+      #   glow!
+      # end
+    end
+    # glow if cryopathy stacks >= 10?
+    # 
+  end
+
+  icon 'Ring of Fire' do
+    action_usable! do
+  end
+
   action_usable 'Comet Storm'
   action_usable 'Glacial Spike' do
     glow!
--- public/examples/warrior/arms.rb
+++ public/examples/warrior/arms.rb
@@ -4,18 +4,41 @@
 # title: 'Warrior: Arms'
 # ---
 
+title 'Arms Warrior'
 load spec: :arms_warrior
 hide_ooc!
 
-dynamic_group 'Arms WhackAuras' do
-  action_usable 'Colossus Smash'
-  # action_usable 'Warbreaker'
-  action_usable 'Execute'
+dynamic_group 'Arms Stay Big' do
+  scale 0.7
+  offset y: -40, x: 60
+
+  action_usable 'Avatar'
   action_usable 'Bladestorm'
-  # TODO: cleave instead of MS display when more than N targets?
-  # action_usable 'Cleave'
-  # action_usable 'Whirlwind'
+end
+
+dynamic_group 'Arms Stay Small' do
+  scale 0.7
+  offset y: -40, x: -60
+
+  action_usable 'Recklessness'
   action_usable 'Thunderous Roar'
+  action_usable 'Colossus Smash'
+end
+
+dynamic_group 'Arms WhackAuras' do
+  scale 0.8
+  offset y: -80
+
+  action_usable 'Skullsplitter'
+  action_usable 'Colossus Smash'
+  action_usable 'Execute' do
+    glow! # todo: glow on sudden death only
+  end
+  action_usable 'Bladestorm'
+  action_usable 'Wrecking Throw'
+  # TODO: cleave instead of MS display when more than N targets?
+  action_usable 'Cleave'
+  # action_usable 'Whirlwind'
 
   # TODO: add `stacks` to glow! instead
   # Min-maxing OP>MS is not recommended.
@@ -27,9 +50,7 @@ dynamic_group 'Arms WhackAuras' do
   # end
 
   action_usable ['Mortal Strike', 'Overpower']
-  action_usable 'Thunder Clap', requires: { target_debuffs_missing: ['Rend'] }
+  # action_usable 'Thunder Clap', requires: { target_debuffs_missing: ['Rend'] }
+  action_usable 'Rend', requires: { target_debuffs_missing: ['Rend'] }
   action_usable 'Sweeping Strikes'
-  action_usable 'Avatar' do
-    glow!
-  end
 end
